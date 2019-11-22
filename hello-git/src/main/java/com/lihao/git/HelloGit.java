c84c9c325c7b37030a6a9bd38a3e59f1e77b220d
lihao <15701151063@163.com>
2019-11-22 13:39:47 +0800

    修改添加

--- hello-git/src/main/java/com/lihao/git/HelloGit.java
+++ hello-git/src/main/java/com/lihao/git/HelloGit.java
@@ -4,6 +4,8 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
 		
-		System.out.println("手动");
+		System.out.println("修改的");
+		
+		System.out.println("新添加的");
     }
 }
