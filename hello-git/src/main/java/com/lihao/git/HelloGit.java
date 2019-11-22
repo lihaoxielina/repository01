310780700148f09724727e845d6001e1b8fbe216
lihao <15701151063@163.com>
2019-11-22 14:13:25 +0800

    DFS

--- hello-git/src/main/java/com/lihao/git/HelloGit.java
+++ hello-git/src/main/java/com/lihao/git/HelloGit.java
@@ -3,7 +3,8 @@ package com.lihao.git;
 public class HelloGit {
     public static void main(String[] args) {
         //手动删除所有后添加输出语句
-		System.out.println("我是一个兵");
+		System.out.println("我是一个兵-------------");
         System.out.println("来自老百姓");
+		
     }
 }
