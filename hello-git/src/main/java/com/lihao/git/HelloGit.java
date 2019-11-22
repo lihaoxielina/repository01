eedd08cef992d20a7eb8a0e484d35aa50b9930c8
lihao <15701151063@163.com>
2019-11-22 14:05:38 +0800

    来自老百姓

--- hello-git/src/main/java/com/lihao/git/HelloGit.java
+++ hello-git/src/main/java/com/lihao/git/HelloGit.java
@@ -2,12 +2,6 @@ package com.lihao.git;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
-		
-		System.out.println("修改的");
-
-		System.out.println("新添加的");
-
-        System.out.println("修改的");
+        System.out.println("来自老百姓");
     }
 }
